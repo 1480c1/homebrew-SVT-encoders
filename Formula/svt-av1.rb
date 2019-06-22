e4f1654d79f63f503c11f14a8e98d1acfc293ed9
Christopher Degawa <ccom@randomderp.com>
2019-06-21 22:06:19 -0500

    SVT-AV1: Add formula file

---
+++ Formula/svt-av1.rb
@@ -0,0 +1,49 @@
+class SvtAv1 < Formula
+  desc "Multicore AV1 Encoder from OpenVisualCloud"
+  homepage "https://github.com/OpenVisualCloud/SVT-AV1"
+  url "https://github.com/OpenVisualCloud/SVT-AV1/archive/v0.5.0.tar.gz"
+  sha256 "cdf36926820d2efcf019ed1a1e66cbc2470b86a28606a00497eb842bd2e495b1"
+  head "https://github.com/OpenVisualCloud/SVT-AV1.git"
+
+  head do
+    url "https://github.com/OpenVisualCloud/SVT-AV1.git"
+  end
+
+  stable do
+    version "0.5.0"
+    patch do
+      url "https://gist.githubusercontent.com/1480c1/19d703727adceb91c09dd0d55a853ff8/raw/0001-CMakeLists-Remove-mavx-and-apply-correct-cflags.patch"
+      sha256 "51b252fe5267a48bc38f61f208c7b3f91d1c6d65b9289585a1fdde9377c7a79c"
+    end
+  end
+
+  depends_on "cmake" => :build
+  depends_on "yasm" => :build
+
+  resource "bus_qcif_15fps.y4m" do
+    url "https://media.xiph.org/video/derf/y4m/bus_qcif_15fps.y4m"
+    sha256 "868fc3446d37d0c6959a48b68906486bd64788b2e795f0e29613cbb1fa73480e"
+  end
+
+  def install
+    mkdir "_build" do
+      system "cmake", "..", *std_cmake_args,
+                      "-DNATIVE=OFF"
+      system "make", "install"
+      prefix.install_metafiles
+    end
+  end
+
+  def caveats
+    "The SVT-AV1 encoder is still not fully stable yet."
+  end
+
+  test do
+    system "#{bin}/SvtAv1EncApp", "-help"
+    resource("bus_qcif_15fps.y4m").stage do
+      system "#{bin}/SvtAv1EncApp", "-enc-mode","8",
+                                    "-i", "bus_qcif_15fps.y4m",
+                                    "-b", "bus_qcif_15fps.ivf"
+    end
+  end
+end
