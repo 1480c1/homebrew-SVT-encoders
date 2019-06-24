577ead500031e1f9eaf796a6f64fc6a8bf79dbff
Christopher Degawa <ccom@randomderp.com>
2019-06-24 01:08:53 -0500

    SVT-AV1: Use branch for default instead of using patches

--- Formula/svt-av1.rb
+++ Formula/svt-av1.rb
@@ -1,25 +1,10 @@
 class SvtAv1 < Formula
   desc "Multicore AV1 Encoder from OpenVisualCloud"
   homepage "https://github.com/OpenVisualCloud/SVT-AV1"
-
+  url "https://github.com/1480c1/SVT-AV1.git",
+      :branch   => "xsave",
+      :revision => "a650a74ad68d18d687c58de2cc0e5170785855b6"
   head "https://github.com/OpenVisualCloud/SVT-AV1.git"
-
-  stable do
-    url "https://github.com/OpenVisualCloud/SVT-AV1/archive/v0.5.0.tar.gz"
-    sha256 "cdf36926820d2efcf019ed1a1e66cbc2470b86a28606a00497eb842bd2e495b1"
-    patch do
-      url "https://raw.githubusercontent.com/1480c1/homebrew-SVT-encoders/master/patches/SVT-AV1/0001-CMakeLists-Prepare-0.5.0-for-packaging.patch"
-      sha256 "cdd15599d3b16201af4c1ee5e8d7a8503248fc586da33df2b8844e63161db07b"
-    end
-    patch do
-      url "https://raw.githubusercontent.com/1480c1/homebrew-SVT-encoders/master/patches/SVT-AV1/0002-Synonyms-only-use-avx-functions-if-MSVC.patch"
-      sha256 "f1d41ca092859b8f592a550eb6816a761b1d43db45e63d7f1de216c65c75f551"
-    end
-    patch do
-      url "https://raw.githubusercontent.com/1480c1/homebrew-SVT-encoders/master/patches/SVT-AV1/0003-IntroPrediction_Intrinsic_AVX2-only-use-avx-function.patch"
-      sha256 "6e04f44553b9a75a7d01bb058a817bc075195fc00915b71ed98a9de2043f2ce1"
-    end
-  end
 
   bottle do
     root_url "https://dl.bintray.com/1480c1/bottles-svt-encoders/"
